282869236e1f1834e68e0a8590d7a5bbb1b1bf87
YiningLiang040129 <156405243+YiningLiang040129@users.noreply.github.com>
2025-02-13 20:10:38 -0800

    Update StudentAI.h

--- src/checkers-cpp/StudentAI.h
+++ src/checkers-cpp/StudentAI.h
@@ -1,53 +1,58 @@
 #ifndef STUDENTAI_H
 #define STUDENTAI_H
+
 #include "AI.h"
 #include "Board.h"
-#pragma once
-
-//The following part should be completed by students.
-//Students can modify anything except the class name and exisiting functions and varibles.
+#include "Move.h"
+#include <vector>
+#include <cmath>
 
 class Node {
 public:
-	Node* parent;
-	vector<Node*> children;
-	vector<Move> unvisitedMoves;
-	int wins = 0;
-	int visits = 0;
-	int player;
-	bool isLeaf = false;
-	Move move;
-	Board board;
-	Node(Node* parent, Move move, Board board, int player) : parent(parent), move(move), board(board), player(player) {}
-	bool isFullyExpanded();
+    Node* parent;
+    std::vector<Node*> children;
+    Move move;
+    Board board;
+    int player;
+    int wins;
+    int visits;
+    bool isLeaf;
+    std::vector<Move> unvisitedMoves;
+
+    Node(Node* parent, Move move, Board board, int player)
+        : parent(parent), move(move), board(board), player(player),
+          wins(0), visits(0), isLeaf(false) {}
+    
+    bool isFullyExpanded();
 };
 
 class MCTS {
 public:
-	Node* root;
-	MCTS(Node* root, Board board, int player);
-	Node* selectNode(Node* node);
-	Node* expandNode(Node* node);
-	int simulation(Node* node);
-	void backPropagation(Node* node, int result);
-	double getUCT(Node* node);
-	void runMCTS(int time);
-	Move getBestMove();
-	bool leadToForceCapture(Board board, Move move, int player);
-	bool causeMoreCaptureByOpponent(Board board, Move move, int player);
-	bool isPromoting(Board board, Move move, int player);
-	double generalBoardPositionEvaluation(Board board, Move move, int player);
-	void deleteTree(Node* node);
-	~MCTS();
+    Node* root;
+
+    MCTS(Node* root, Board board, int player);
+    
+    Node* selectNode(Node* node);
+    Node* expandNode(Node* node);
+    int simulation(Node* node);
+    void backPropagation(Node* node, int result);
+    double getUCT(Node* node);
+    void runMCTS(int time);
+    Move getBestMove();
+
+    double evaluateBoard(Board board, int player);
+
+    void deleteTree(Node* node);
+    ~MCTS();
 };
 
-
-class StudentAI :public AI
-{
+class StudentAI : public AI {
 public:
     Board board;
-	StudentAI(int col, int row, int p);
-	virtual Move GetMove(Move board);
+    int player;
+    
+    StudentAI(int col, int row, int p);
+    Move GetMove(Move move) override;
 };
 
-#endif //STUDENTAI_H
+#endif // STUDENTAI_H
